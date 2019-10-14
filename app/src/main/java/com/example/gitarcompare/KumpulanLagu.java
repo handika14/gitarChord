2f0dfb0868acce9a05805db8ed938ef1dd71a2ae
handika14 <achmadhandika0@gmail.com>
2019-10-14 18:56:26 +0700

    Menambahkan onclick, lirik dan chord pada top chart lagu(Kumpulan chord lagu)

--- app/src/main/java/com/example/gitarcompare/KumpulanLagu.java
+++ app/src/main/java/com/example/gitarcompare/KumpulanLagu.java
@@ -2,7 +2,9 @@ package com.example.gitarcompare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class KumpulanLagu extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class KumpulanLagu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kumpulan_lagu);
+    }
+
+    public void lagu1(View view) {
+        Intent intent =  new Intent(this, lagu1.class);
+        startActivity(intent);
+    }
+
+    public void lagu2(View view) {
+        Intent intent =  new Intent(this, lagu2.class);
+        startActivity(intent);
+    }
+
+    public void lagu3(View view) {
+        Intent intent =  new Intent(this, lagu3.class);
+        startActivity(intent);
+    }
+
+    public void lagu4(View view) {
+        Intent intent =  new Intent(this, lagu4.class);
+        startActivity(intent);
+    }
+
+    public void lagu5(View view) {
+        Intent intent =  new Intent(this, lagu5.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/gitarcompare/lagu5.java
@@ -0,0 +1,14 @@
+package com.example.gitarcompare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lagu5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lagu5);
+    }
+}
