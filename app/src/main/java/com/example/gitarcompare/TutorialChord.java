614efca8c0b74b8d9ffcbd7efc30ac8d9919851f
handika14 <achmadhandika0@gmail.com>
2019-10-16 15:00:46 +0700

    Menambahkan fragment tutorial chord(Tutorial chord lagu)

--- app/src/main/java/com/example/gitarcompare/TutorialChord.java
+++ app/src/main/java/com/example/gitarcompare/TutorialChord.java
@@ -1,8 +1,13 @@
 package com.example.gitarcompare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.gitarcompare.fragments.ChordFragment;
+import com.example.gitarcompare.fragments.VideoFragment;
 
 public class TutorialChord extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class TutorialChord extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_chord);
+    }
+
+    public void chordFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        ChordFragment chordfragment = (ChordFragment) getSupportFragmentManager().findFragmentByTag("CHORD_FRAGMENT");
+        if(chordfragment != null && chordfragment.isVisible()){
+            fragmentTransaction.commit();
+        }else {
+            fragmentTransaction.setCustomAnimations(R.anim.exit_from_right, R.anim.exit_from_left);
+            fragmentTransaction.replace(R.id.tutorial_fragment,new ChordFragment(),"CHORD_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void videoFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        VideoFragment videoFragment = (VideoFragment) getSupportFragmentManager().findFragmentByTag("VIDEO_FRAGMENT");
+        if(videoFragment != null && videoFragment.isVisible()){
+            fragmentTransaction.commit();
+        }else {
+            fragmentTransaction.setCustomAnimations(R.anim.exit_from_right, R.anim.exit_from_left);
+            fragmentTransaction.replace(R.id.tutorial_fragment,new VideoFragment(),"VIDEO_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
